994fde5f5b00a5ec613d8e8b99e2bc604ef3ae99
Anna Hansen <ahansen@pdx.edu>
2021-11-30 10:08:01 -0800

    working on calling query from view.py, using values returned, updating view.html

--- components/namesquery.py
+++ components/namesquery.py
@@ -2,71 +2,75 @@ from google.cloud import bigquery
 import pandas as pd
 import numpy as np
 
+def getNamesData(name, year, gender):
 
-name = 'Anna'
-year = '1985'
-gender = 'F'
+    # values used for testing queries
+    # name = 'Anna'
+    # year = '1985'
+    # gender = 'F'
 
-client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
+    # instantiate the bq client
+    client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
+    
+    # the big query public dataset I am querying
+    dataset = 'bigquery-public-data.usa_names.usa_1910_2013'
 
-dataset = 'bigquery-public-data.usa_names.usa_1910_2013'
-
-# ----------------------------------------------------
-#           get the top name for the year
-# ----------------------------------------------------
-topNameQueryString = (
-    f'SELECT name '
-    f'FROM `{dataset}` '
-    f'WHERE gender="{gender}" '
-    f'AND year={year} '
-    f'GROUP BY name '
-    f'ORDER BY sum(number) DESC '
-    f'LIMIT 1'
-)
-
-# results = client.query(topNameQueryString)
-
-# # I would like to directly access the row rather than iterating over the object, but I can't find out how. This approach assumes there will only be one row, which only works because my query results are limited to one row. If there were more than one row this would not work as-is.
-# for row in results:
-#     topName = row['name']
-
-# ----------------------------------------------------
-#           get the rank of the name
-# ----------------------------------------------------
-nameRankQueryString = (
-    f'SELECT ranking '
-    f'FROM (SELECT name, sum(number), RANK() OVER (ORDER BY SUM(number) DESC) as  ranking '
+    # ----------------------------------------------------
+    #           get the top name for the year
+    # ----------------------------------------------------
+    topNameQueryString = (
+        f'SELECT name '
         f'FROM `{dataset}` '
         f'WHERE gender="{gender}" '
         f'AND year={year} '
         f'GROUP BY name '
-        f'ORDER BY sum(number) DESC) '
-    f'WHERE name="{name}" '
-)
+        f'ORDER BY sum(number) DESC '
+        f'LIMIT 1'
+    )
 
-results = client.query(nameRankQueryString)
+    results = client.query(topNameQueryString)
 
-# This has the same problem described above. 
-for row in results:
-    nameRank = row['ranking']
+    # I would like to directly access the row rather than iterating over the object, but I can't find out how. This approach assumes there will only be one row, which only works because my query results are limited to one row. If there were more than one row this would not work as-is.
+    for row in results:
+        topName = row['name']
 
-# ----------------------------------------------------
-#           get data for the chart of the name
-# ----------------------------------------------------
-chartDataQueryString = (
-    f'SELECT year, sum(number) as total '
-    f'FROM `{dataset}` '
-    f'WHERE gender="{gender}" '
-    f'AND name="{name}" '
-    f'GROUP BY name, year '
-    f'ORDER BY year '
-)
+    # ----------------------------------------------------
+    #           get the rank of the name
+    # ----------------------------------------------------
+    nameRankQueryString = (
+        f'SELECT ranking '
+        f'FROM (SELECT name, sum(number), RANK() OVER (ORDER BY SUM(number) DESC) as  ranking '
+            f'FROM `{dataset}` '
+            f'WHERE gender="{gender}" '
+            f'AND year={year} '
+            f'GROUP BY name '
+            f'ORDER BY sum(number) DESC) '
+        f'WHERE name="{name}" '
+    )
 
-df = client.query(chartDataQueryString).to_dataframe()
+    results = client.query(nameRankQueryString)
 
-# print(topName)
-# print(nameRank)
-print(df.head())
+    # This has the same problem described above. 
+    for row in results:
+        nameRank = row['ranking']
 
+    # ----------------------------------------------------
+    #           get data for the chart of the name
+    # ----------------------------------------------------
+    chartDataQueryString = (
+        f'SELECT year, sum(number) as total '
+        f'FROM `{dataset}` '
+        f'WHERE gender="{gender}" '
+        f'AND name="{name}" '
+        f'GROUP BY name, year '
+        f'ORDER BY year '
+    )
+
+    df = client.query(chartDataQueryString).to_dataframe()
+
+    chartURL = 'https://placekitten.com/200/300'
+    #TODO need to create chart and save it somewhere so that I can return the url to the page for rendering
+
+    return topName, nameRank, chartURL
 
 
--- view.py
+++ view.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
 from flask.views import MethodView
+from /components/namesquery import getNamesData
 
 class View(MethodView):
     def post(self):
@@ -10,9 +11,13 @@ class View(MethodView):
         # request.form['name']
         # request.form['year']
         # request.form['gender']
+        name = request.form['name']
+        year = request.form['year']
+        gender = request.form['gender']
 
+        topName, nameRank, chartURL = getNamesData(name, year, gender)
+        #TODO handle empty values
 
-
-        data = dict(name=request.form['name'], year=request.form['year'], gender=request.form['gender'],nameRank='post rank', popularName='post popular name', topMovie='post popular movie', topSong='post popular song')
+        data = dict(name=name, year=year, gender=gender, nameRank=nameRank, popularName=topName, topMovie='post popular movie', topSong='post popular song', chartURL=chartURL)
         
         return render_template('view.html', data=data)
