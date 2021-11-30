b1de98558e774b828c0d68e8712c90777be27e3c
Anna Hansen <ahansen@pdx.edu>
2021-11-30 15:13:46 -0800

    public image saved to gcs working

--- chart.py
+++ chart.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery
+from google.cloud import storage
 import pandas
 import numpy
 import matplotlib
@@ -9,9 +10,9 @@ import matplotlib
 # values used for testing queries
 name = 'Anna'
 gender = 'F'
-
+filename = f'{name}-chart.jpg'
 # instantiate the bq client
-client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
+bq_client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
     
 # the big query public dataset I am querying
 dataset = 'bigquery-public-data.usa_names.usa_1910_2013'
@@ -31,11 +32,22 @@ chartDataQueryString = (
     f'ORDER BY year '
 )
 
-df = client.query(chartDataQueryString).to_dataframe()
+df = bq_client.query(chartDataQueryString).to_dataframe()
 plot = df.plot(x='year', y='total', title=f'Babies Named {name} 1910-2013', legend=False) #kind='line' is default
 fig = plot.get_figure()
-fig.savefig("output.jpg")
+fig.savefig(filename)
 
 #TODO need to create chart and save it somewhere then set chartURL to the url
 
+#save to gcs bucket
+bucket_name = 'ahansen-finalproject'
+object_name = filename
+
+storage_client = storage.Client('cloud-f21-anna-hansen-ahansen')
+bucket = storage_client.bucket(bucket_name)
+blob = bucket.blob(object_name)
+blob.upload_from_filename(object_name)
+blob.make_public()
+chartURL = f'storage.cloud.google.com/{bucket_name}/{object_name}'
+print(chartURL)
 # return chartURL
