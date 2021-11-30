06b1c27df6066096e83af994d5776ad5ce55c6cd
Anna Hansen <ahansen@pdx.edu>
2021-11-30 11:01:01 -0800

    calling query as function working, using chartURL in view.html working

--- view.py
+++ view.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from flask.views import MethodView
-from /components/namesquery import getNamesData
+from namesquery import getNamesData
 
 class View(MethodView):
     def post(self):
@@ -17,6 +17,12 @@ class View(MethodView):
 
         topName, nameRank, chartURL = getNamesData(name, year, gender)
         #TODO handle empty values
+
+        # re-do gender for display on page
+        if gender == 'M':
+            gender = 'male'
+        else:
+            gender = 'female'
 
         data = dict(name=name, year=year, gender=gender, nameRank=nameRank, popularName=topName, topMovie='post popular movie', topSong='post popular song', chartURL=chartURL)
         
