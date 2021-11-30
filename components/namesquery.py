d735ebfe76b3b1dfb1918db0a2d04da96197e063
Anna Hansen <ahansen@pdx.edu>
2021-11-30 09:54:20 -0800

    bigquery queries are working

--- components/namesquery.py
+++ components/namesquery.py
@@ -1,53 +1,72 @@
 from google.cloud import bigquery
+import pandas as pd
+import numpy as np
 
-def namesquery():
-    # for production put name, year, gender into function arguments
-    name = 'Anna'
-    year = '1985'
-    gender = 'F'
-    
-    client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
 
-    dataset = 'bigquery-public-data.usa_names.usa_1910_2013'
+name = 'Anna'
+year = '1985'
+gender = 'F'
 
-    topNameQueryString = (
-        f'SELECT name'
-        f'FROM {dataset}'
-        f'WHERE gender={gender}'
-        f'AND year={year}'
-        f'GROUP BY name'
-        f'ORDER BY sum(number) DESC'
-        f'LIMIT 1'
-    )
+client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
 
-    nameRankQueryString = (
-        f'SELECT ranking'
-        f'FROM (SELECT name, sum(number), RANK() OVER (ORDER BY SUM(number) DESC) as  ranking'
-            f'FROM {dataset}'
-            f'WHERE gender={gender}'
-            f'AND year={year}'
-            f'GROUP BY name'
-            f'ORDER BY sum(number) DESC)'
-        f'WHERE name={name}'
-    )
+dataset = 'bigquery-public-data.usa_names.usa_1910_2013'
 
-    chartDataQueryString = (
-        f'SELECT year, sum(number) as total'
-        f'FROM {dataset}'
-        f'WHERE gender={gender}'
-        f'AND name={name}'
-        f'GROUP BY name, year'
-        f'ORDER BY year'
-    )
+# ----------------------------------------------------
+#           get the top name for the year
+# ----------------------------------------------------
+topNameQueryString = (
+    f'SELECT name '
+    f'FROM `{dataset}` '
+    f'WHERE gender="{gender}" '
+    f'AND year={year} '
+    f'GROUP BY name '
+    f'ORDER BY sum(number) DESC '
+    f'LIMIT 1'
+)
 
-    topNameQueryJob = client.query(topNameQueryString)
-    nameRankQueryJob = client.query(nameRankQueryString)
-    chartDataQueryJob = client.query(chartDataQueryString)
+# results = client.query(topNameQueryString)
 
-    topName = topNameQueryJob.result()
-    nameRank = nameRankQueryJob.result()
-    chartData = chartDataQueryJob.result()
+# # I would like to directly access the row rather than iterating over the object, but I can't find out how. This approach assumes there will only be one row, which only works because my query results are limited to one row. If there were more than one row this would not work as-is.
+# for row in results:
+#     topName = row['name']
 
-    print(topName)
+# ----------------------------------------------------
+#           get the rank of the name
+# ----------------------------------------------------
+nameRankQueryString = (
+    f'SELECT ranking '
+    f'FROM (SELECT name, sum(number), RANK() OVER (ORDER BY SUM(number) DESC) as  ranking '
+        f'FROM `{dataset}` '
+        f'WHERE gender="{gender}" '
+        f'AND year={year} '
+        f'GROUP BY name '
+        f'ORDER BY sum(number) DESC) '
+    f'WHERE name="{name}" '
+)
 
-    return ([name, year])
+results = client.query(nameRankQueryString)
+
+# This has the same problem described above. 
+for row in results:
+    nameRank = row['ranking']
+
+# ----------------------------------------------------
+#           get data for the chart of the name
+# ----------------------------------------------------
+chartDataQueryString = (
+    f'SELECT year, sum(number) as total '
+    f'FROM `{dataset}` '
+    f'WHERE gender="{gender}" '
+    f'AND name="{name}" '
+    f'GROUP BY name, year '
+    f'ORDER BY year '
+)
+
+df = client.query(chartDataQueryString).to_dataframe()
+
+# print(topName)
+# print(nameRank)
+print(df.head())
+
+
+
