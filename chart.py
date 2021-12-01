cbf304d66aed58239c4a8d2795c9a30f7cd2e580
Anna Hansen <ahansen@pdx.edu>
2021-11-30 16:24:57 -0800

    getChart working, image served on gcs working

--- chart.py
+++ chart.py
@@ -3,51 +3,69 @@ from google.cloud import storage
 import pandas
 import numpy
 import matplotlib
+import os
 
 
-# def getChart(name, gender):
-
-# values used for testing queries
-name = 'Anna'
-gender = 'F'
-filename = f'{name}-chart.jpg'
-# instantiate the bq client
-bq_client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
+def getChart(name, gender):
+    # ----------------------------------------------------
+    #           get data for the chart of the name
+    # ----------------------------------------------------
     
-# the big query public dataset I am querying
-dataset = 'bigquery-public-data.usa_names.usa_1910_2013'
+    # instantiate the bq client
+    bq_client = bigquery.Client('cloud-f21-anna-hansen-ahansen')
+        
+    # the big query public dataset I am querying
+    dataset = 'bigquery-public-data.usa_names.usa_1910_2013'
+    
+    # query string
+    chartDataQueryString = (
+        f'SELECT year, sum(number) as total '
+        f'FROM `{dataset}` '
+        f'WHERE gender="{gender}" '
+        f'AND name="{name}" '
+        f'GROUP BY name, year '
+        f'ORDER BY year '
+    )
 
-# placeholder image will be updated later with real url
-chartURL = 'https://placekitten.com/200/300'
+    # run query
+    job = bq_client.query(chartDataQueryString)
+    
+    # test how many rows were returned
+    rows = job.result()
+    if rows.total_rows < 1:
+        # no data in table
+        chartURL = ''
+        return chartURL
+    else:
+        # ----------------------------------------------------
+        #           generate and save chart image
+        # ----------------------------------------------------
+        filename = f'{name}-chart.jpg'
 
-# ----------------------------------------------------
-#           get data for the chart of the name
-# ----------------------------------------------------
-chartDataQueryString = (
-    f'SELECT year, sum(number) as total '
-    f'FROM `{dataset}` '
-    f'WHERE gender="{gender}" '
-    f'AND name="{name}" '
-    f'GROUP BY name, year '
-    f'ORDER BY year '
-)
+        df = job.to_dataframe()
+        plot = df.plot(x='year', y='total', title=f'Babies Named {name} 1910-2013', legend=False) #kind='line' is default
+        fig = plot.get_figure()
+        fig.savefig(filename)
 
-df = bq_client.query(chartDataQueryString).to_dataframe()
-plot = df.plot(x='year', y='total', title=f'Babies Named {name} 1910-2013', legend=False) #kind='line' is default
-fig = plot.get_figure()
-fig.savefig(filename)
+        # ----------------------------------------------------
+        #           upload to storage bucket
+        # ----------------------------------------------------
+        bucket_name = 'ahansen-finalproject'
+        object_name = filename
 
-#TODO need to create chart and save it somewhere then set chartURL to the url
+        # instantiate the storage client
+        storage_client = storage.Client('cloud-f21-anna-hansen-ahansen')
+        
+        # access the bucket and save the file to it publicly
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(object_name)
+        blob.upload_from_filename(object_name)
+        blob.make_public()
+        
+        chartURL = f'https://storage.cloud.google.com/{bucket_name}/{object_name}'
 
-#save to gcs bucket
-bucket_name = 'ahansen-finalproject'
-object_name = filename
-
-storage_client = storage.Client('cloud-f21-anna-hansen-ahansen')
-bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(object_name)
-blob.upload_from_filename(object_name)
-blob.make_public()
-chartURL = f'storage.cloud.google.com/{bucket_name}/{object_name}'
-print(chartURL)
-# return chartURL
+        # cleanup - delete local copy of file
+        if os.path.exists(filename):
+            os.remove(filename)
+        
+        return chartURL
